d8bda4e0e6e100b4114a9734293827cf99a5af37
Angel Estevez <angelestevez@MacBook-Air-de-Angel.local>
2023-11-04 01:33:12 -0600

    cart counter

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,7 +1,10 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { ShoppingCartContext } from "../../Context";
 
 const Navbar = () =>{
 
+    const context = useContext(ShoppingCartContext)
     const activeStyle = 'underline underline-offset-4'
 
     return(
@@ -72,7 +75,7 @@ const Navbar = () =>{
                     </NavLink>
                 </li>
                 <li>
-                    🛒 0
+                    🛒 {context.count}
                 </li>
             </ul>
         </nav>
