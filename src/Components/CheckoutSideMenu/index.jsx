2948b18bf735d9ea32282eaf6a03427b189a3407
Angel Estevez <angelestevez@MacBook-Air-de-Angel.local>
2023-11-04 21:31:41 -0600

    delete product

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -2,9 +2,15 @@ import { XMarkIcon } from '@heroicons/react/24/solid'
 import './styles.css'
 import { useContext } from 'react'
 import { ShoppingCartContext } from '../../Context'
+import OrderCard from '../OrderCard'
 
 const CheckoutSideMenu = ()=>{
     const context = useContext(ShoppingCartContext)
+
+    const handleDelete = (id)=>{
+        const filteredProducts = context.cartProducts.filter(product => product.id != id)
+        context.setCartProducts(filteredProducts)
+    }
 
     return (
         <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}>
@@ -13,6 +19,13 @@ const CheckoutSideMenu = ()=>{
                 <div>
                     <XMarkIcon className="h-6 w-6 text-black cursor-pointer" onClick={() => context.closeCheckoutSideMenu()}/>
                 </div>
+            </div>
+            <div className='px-6 overflow-y-scroll'>
+                {
+                    context.cartProducts.map(product=>(
+                        <OrderCard key={product.id} id={product.id} title={product.title} imageURL={product.images} price={`$${product.price}`} handleDelete={handleDelete} />
+                    ))
+                }
             </div>
         </aside>
     )
