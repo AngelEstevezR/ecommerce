3959fe5f10af040e163f512c2a1db2a983083b11
Angel Estevez <angelestevez@MacBook-Air-de-Angel.local>
2023-11-05 17:43:49 -0600

    prueba

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -24,7 +24,7 @@ function Home() {
   return (
     <Layout>
       <div className="flex items-center justify-center relative w-80 mb-4">
-        <h1 className="font-medium text-xl">Exclusive Products</h1>
+        <h1 className="font-medium text-xl">Exclusive Products!</h1>
       </div>
       <input type="text" placeholder="Search a product" className="rounded-lg border border-black w-80 p-4 mb-4 focus:outline-none" 
       onChange={(event)=> context.setSearchByTitle(event.target.value)}/>
